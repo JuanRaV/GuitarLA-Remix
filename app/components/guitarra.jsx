f1b5cbf1ab3c7cb43e991ccf71eecb7a130ea1fc
JuanRaV <juanravirgen@gmail.com>
2023-09-17 21:19:51 -0600

    Getting guitars from my API

---
+++ app/components/guitarra.jsx
@@ -0,0 +1,10 @@
+const Guitarra = ({guitarra}) => {
+    console.log(guitarra)
+  return (
+    <div>
+      <p>Guitarra</p>
+    </div>
+  )
+}
+
+export default Guitarra
--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,8 +1,31 @@
+import { useLoaderData } from "@remix-run/react";
+import { getGuitarras } from "../../.cache/models/guitarras.server"
+import Guitarra from "../components/guitarra";
+
+export async function loader(){
+  const guitarras = await getGuitarras();
+  return guitarras.data
+}
+
 const Tienda = () => {
+  const guitarras =useLoaderData()
+
   return (
-    <div>
-      Desde Tienda
-    </div>
+    <main className='contenedor'>
+      
+      <h2 className='heading'>Nuestra Colleccion</h2>
+      
+      {guitarras?.length && (
+          <div className='guitarras-grid'>
+          {guitarras.map((guitarra) => (
+           <Guitarra 
+            key={guitarra?.attributes.url}
+            guitarra={guitarra?.attributes}
+           />
+          ))}
+       </div>
+      )}
+    </main>
   )
 }
 
