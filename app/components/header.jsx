3523be81b1b6d046dc1aa9b87a23eb48372e28e8
JuanRaV <juanravirgen@gmail.com>
2023-09-17 19:12:51 -0600

    Finishing header

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,8 +1,36 @@
+import { Link,useLocation } from "@remix-run/react"
+import logo from '../../public/img/logo.svg'
+
 const Header = () => {
+    const location = useLocation()
+    console.log(location.pathname)
   return (
-    <div>
-      Header
-    </div>
+    <header className="header">
+        <div className="contenedor barra">
+            <Link to="/">
+                <img className="logo" src={logo} alt="Imagen Logo" />
+            </Link>
+            <nav className="navegacion">
+                <Link
+                    to='/'
+                    className={location.pathname==='/'? 'active' : ''}
+                >Inicio</Link>
+                <Link
+                    to='/nosotros'
+                    className={location.pathname==='/nosotros'? 'active' : ''}
+                >Nosotros</Link>
+                <Link
+                    to='/tienda'
+                    className={location.pathname==='/tienda'? 'active' : ''}
+                >Tienda</Link>
+                <Link
+                    to='/blog'
+                    className={location.pathname==='/blog'? 'active' : ''}
+                >Blog</Link>        
+            </nav>
+        </div>
+
+    </header>
   )
 }
 
