f2099c04fdb832b0520d78ad7aa722027639e48e
JuanRaV <juanravirgen@gmail.com>
2023-09-18 15:07:58 -0600

    Showing guitar info and changing meta

--- app/routes/guitarras.$guitarraUrl.jsx
+++ app/routes/guitarras.$guitarraUrl.jsx
@@ -1,14 +1,42 @@
-export async function loader({request,params}){
-    console.log('request',request)
-    console.log('params',params)
-    return {}
+import { useLoaderData } from "@remix-run/react"
+import { getGuitarra } from "../../.cache/models/guitarras.server"
+import styles from '../styles/guitarras.css'
+
+export function meta({data}){
+  return [
+    {
+      title: `GuitarLA - ${data.data[0].attributes.nombre}`,
+      descripcion: `Venta de guitarras - ${data.data[0].attributes.nombre}`
+    }
+  ]
+}
+export function links(){
+  return [
+    {
+      rel:'stylesheet',
+      href:styles
+    }
+  ]
+}
+export async function loader({params}){
+    const { guitarraUrl} = params
+    const guitarra = await getGuitarra(guitarraUrl)
+    return guitarra
 }
 
-const Guitarra = () => {
+const Guitarra = () => { 
+
+  const guitarra = useLoaderData()
+  const {nombre,descripcion,imagen,precio} = guitarra.data[0].attributes
   return (
-    <div>
-      Guitarra URL
-    </div>
+    <main className="contenedor guitarra"> 
+      <img className="imagen" src={imagen.data.attributes.url} alt={`Imgen de ${nombre}`} />
+      <div className="contenido">
+        <h3>{nombre}</h3>
+        <p className="texto">{descripcion}</p>
+        <p className="precio">${precio}</p>
+      </div>
+    </main> 
   )
 }
 
