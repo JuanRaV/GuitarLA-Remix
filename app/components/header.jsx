90ed6c036e698e345eeeb12a77398bf293f00c2a
JuanRaV <juanravirgen@gmail.com>
2023-09-17 19:30:54 -0600

    Finishing the footer

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,33 +1,15 @@
-import { Link,useLocation } from "@remix-run/react"
+import { Link } from "@remix-run/react"
 import logo from '../../public/img/logo.svg'
+import Navegation from "./navegation"
 
 const Header = () => {
-    const location = useLocation()
-    console.log(location.pathname)
   return (
     <header className="header">
         <div className="contenedor barra">
             <Link to="/">
                 <img className="logo" src={logo} alt="Imagen Logo" />
             </Link>
-            <nav className="navegacion">
-                <Link
-                    to='/'
-                    className={location.pathname==='/'? 'active' : ''}
-                >Inicio</Link>
-                <Link
-                    to='/nosotros'
-                    className={location.pathname==='/nosotros'? 'active' : ''}
-                >Nosotros</Link>
-                <Link
-                    to='/tienda'
-                    className={location.pathname==='/tienda'? 'active' : ''}
-                >Tienda</Link>
-                <Link
-                    to='/blog'
-                    className={location.pathname==='/blog'? 'active' : ''}
-                >Blog</Link>        
-            </nav>
+            <Navegation/>
         </div>
 
     </header>
