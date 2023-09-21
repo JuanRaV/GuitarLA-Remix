fd92f5d4aa55c3d9caf1eee8abf0eb5e4129f89e
JuanRaV <juanravirgen@gmail.com>
2023-09-21 11:27:46 -0600

    Avoiding repeted elements on the carrito

--- app/root.jsx
+++ app/root.jsx
@@ -51,7 +51,22 @@ export default function App(){
     const [carrito,setCarrito] = useState([])
 
     const agregarCarrito = guitarra=>{
-        setCarrito([...carrito,guitarra])
+        //Si ya esta la misma guitarra en el carrito evitaremos registros duplicados
+        if(carrito.some(guitarraState=> guitarraState.id === guitarra.id)){
+            //Iterar sobre el arreglo e identificar el elemento duplicado
+            const carritoActualizado = carrito.map(guitarraState=>{
+                if(guitarraState.id === guitarra.id){
+                    //Reescribir la cantidad
+                    guitarraState.cantidad = guitarra.cantidad
+                }
+                return guitarraState
+            })
+            //Anadir al carrito
+            setCarrito(carritoActualizado)
+        }else{
+            //Registro nuevo, agregar al carrito
+            setCarrito([...carrito,guitarra])
+        }
     }
     return (
         <Document>
